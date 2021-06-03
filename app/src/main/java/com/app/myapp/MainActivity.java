668b29e11ed06dd51e6ddcd8e0a009b7d7c6e45c
XiongShao <jerry29221979@gmail.com>
2021-06-03 13:51:44 +0800

    Initial commit

---
+++ app/src/main/java/com/app/myapp/MainActivity.java
@@ -0,0 +1,67 @@
+package com.app.myapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button linear, grid, stagger;
+    RecyclerView recyclerView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        linear  = findViewById(R.id.linearButton);
+        grid    = findViewById(R.id.gridButton);
+        stagger = findViewById(R.id.staggerButton);
+
+
+        // 把項目清單準備好，放在一個List物件裏頭
+        List<String> listStr = new ArrayList<>();
+        for (int i = 0; i < 50; i++)
+            listStr.add(new String("第" + String.valueOf(i+1) + "項"));
+
+        // 取得介面佈局檔中的RecyclerView元件
+        recyclerView = findViewById(R.id.recyclerView);
+
+        // 設定RecyclerView使用的LayoutManager，
+        // LayoutManager決定項目的排列方式。
+        linear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                recyclerView.setLayoutManager(new LinearLayoutManager(MainActivity.this));
+            }
+        });
+
+        grid.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                recyclerView.setLayoutManager(new GridLayoutManager(MainActivity.this, 2));
+            }
+        });
+
+        stagger.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                recyclerView.setLayoutManager(new StaggeredGridLayoutManager(3,
+                        StaggeredGridLayoutManager.VERTICAL));
+            }
+        });
+
+        // 建立RecyclerView的Adapter物件，傳入包含項目清單的List物件
+        RecyclerViewAdapter adapter = new RecyclerViewAdapter(listStr);
+
+        // 把Adapter物件傳給RecyclerView
+        recyclerView.setAdapter(adapter);
+    }
+}
---
+++ app/src/main/java/com/app/myapp/RecyclerViewAdapter.java
@@ -0,0 +1,81 @@
+package com.app.myapp;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.List;
+import java.util.Random;
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder> {
+
+    // 儲存要顯示的資料。
+    private List<String> mListString;
+    private int[] images = {android.R.drawable.star_on, R.drawable.item01, R.drawable.item02};
+    // ViewHolder 是把項目中所有的 View 物件包起來。
+    // 它在 onCreateViewHolder() 中使用。
+    public class ViewHolder extends RecyclerView.ViewHolder
+            implements View.OnClickListener {
+        public ImageView mImgView;
+        public TextView mTxt;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            mImgView = (ImageView) itemView.findViewById(R.id.imgView);
+            mTxt = (TextView) itemView.findViewById(R.id.txt);
+
+            // 處理按下的事件。
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+            // 按下後執行的程式碼。
+            Toast.makeText(view.getContext(),
+                    mListString.get(getAdapterPosition()), Toast.LENGTH_LONG)
+                    .show();
+        }
+    }
+
+    // 建構式，用來接收外部程式傳入的項目資料。
+    public RecyclerViewAdapter(List<String> listString) {
+        mListString = listString;
+    }
+
+    // RecyclerView會呼叫這個方法，我們必須建立好項目的ViewHolder物件，
+    // 然後傳回給RecyclerView。
+    @NonNull
+    @Override
+    public RecyclerViewAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        // 建立一個 view。
+        View v = LayoutInflater.from(viewGroup.getContext()).inflate(
+                R.layout.recycler_view_item, viewGroup, false);
+
+        // 建立這個 view 的 ViewHolder。
+        ViewHolder viewHolder = new ViewHolder(v);
+        return viewHolder;
+    }
+
+    // RecyclerView會呼叫這個方法，我們必須把項目資料填入ViewHolder物件。
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerViewAdapter.ViewHolder viewHolder, int i) {
+        // 把資料設定給 ViewHolder。
+        Random rand = new Random();
+        int x = rand.nextInt(3);
+        Log.d("x= ", String.valueOf(x));
+        viewHolder.mImgView.setImageResource(images[x]);
+        viewHolder.mTxt.setText(mListString.get(i));
+    }
+
+    // RecyclerView會呼叫這個方法，我們要傳回總共有幾個項目。
+    @Override
+    public int getItemCount() {
+        return mListString.size();
+    }
+}
